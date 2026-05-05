07d243d28d18427808c62bd976c337272e4a8f05
Klein Magallano <kleinalboriamagallano@gmail.com>
2026-05-05 22:09:51 +0800

    ite14 project

--- ite14_files/graphs/main.c
+++ ite14_files/graphs/main.c
@@ -1,82 +1,260 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct node {
-    char value;
-    struct node * adj;
-} Node;
+typedef struct Edge {
+    char dest;
+    int weight;
+    struct Edge * next;
+} Edge;
+typedef struct Vertex {
+    char value;         // value of the vertex
+    int numOfEdges;   
+    Edge * edges;
+} Vertex;
+typedef struct Graph {
+       
+    int numOfVertices;   
+    Vertex ** vertices; 
+} Graph;
 
-Node * createNode(char value) {
-    Node * newNode = (Node *)malloc(sizeof(Node));
-    if(newNode == NULL) {
-        printf("error: failed to allocate memory for newNode in createNode().\n");
+Graph * createGraph() {
+    Graph * newGraph = (Graph *)malloc(sizeof(Graph));
+    if(newGraph == NULL) {
+        printf("Error: failed to allocate memory for newGraph in createGraph().\n");
         return NULL;
     }
-    newNode->value = value;
-    newNode->adj = newNode;
-    return newNode;
+    newGraph->numOfVertices = 0;
+    newGraph->vertices = (Vertex **)malloc(sizeof(Vertex *));
+    return newGraph;
 }
 
-void connectNode(Node * src, Node * dest) {
-    if(src == NULL) {
-        printf("error: src Node is NULL in connectNode().\n");
-        return;
+Vertex * createVertex(char value) {
+    Vertex * newVertex = (Vertex *)malloc(sizeof(Vertex));
+    if(newVertex == NULL) {
+        printf("Error: failed to allocate memory for newVertex in createVertex().\n");
+        return NULL;
     }
-    else if(dest == NULL) {
-        printf("error: dest Node is NULL in connectNode().\n");
-        return;
-    }
-    else {
-        printf("error: An error occured in connectNode().\n");
-        return;
-    }
-
-    src->adj = dest;
-    dest->adj = dest;
+    newVertex->value = value;
+    newVertex->edges = NULL;
+    newVertex->numOfEdges = 0;
+    return newVertex;
 }
 
-void insertNodeList(Node *** nodeList, int * size, char value) {
-    if(*nodeList == NULL) {
-        *nodeList = (Node **)malloc(sizeof(Node *));
-        if(*nodeList == NULL) {
-            printf("error: Failed to allocate memory in insertNodeList.\n");
-            return;
-        }
-        **nodeList = createNode(value);
-        (*size)++;
+Edge * createEdge(char dest, int weight) {
+    Edge * newEdge = (Edge *)malloc(sizeof(Edge));
+    if(newEdge == NULL) {
+        printf("Error: failed to allocate memory for newEdge in createEdge().\n");
+        return NULL;
+    }
+    newEdge->dest = dest;
+    newEdge->weight = weight;
+    newEdge->next = NULL;
+    return newEdge;
+}
+
+
+bool isExistVertex(Graph * graph, char target) {
+    if(graph == NULL) {
+        printf("Error: graph is null when isExistVertex().\n");
+        return false;
+    }
+    int i;
+    for(i = 0; i < graph->numOfVertices; i++) {
+        if((graph->vertices)[i]->value == target)
+            return true;
+    }
+    return false;
+}
+
+Vertex * searchVertex(Graph * graph, char target) {
+    if(graph == NULL) {
+        printf("Error: graph is null when searchVertex.\n");
+        return NULL;
+    }
+
+    if(!isExistVertex(graph, target)) {
+        printf("Error: target %c does not exist when searchVertex().\n", target);
+        return NULL;
+    }
+
+    int i;
+    for(i = 0; i < graph->numOfVertices; i++) {
+        if((graph->vertices)[i]->value == target)
+            return (graph->vertices)[i];
+    }
+    return NULL;
+}
+
+bool isExistEdge(Graph * graph, char vertex, char dest){
+    if(graph == NULL) {
+        printf("Error: graph is null when isExistEdge().\n");
+        return false;
+    }
+
+    Vertex * targetVertex = searchVertex(graph, vertex);
+    if(targetVertex == NULL)
+        return false;
+    
+    Edge * edgeStart = targetVertex->edges;
+    while(edgeStart != NULL){
+        if(edgeStart->dest == dest)
+            return true;
+        edgeStart = edgeStart->next;
+    }
+    return false;
+}
+
+void insertVertex(Graph * graph, char value) {
+    if(graph == NULL) {
+        printf("Error: graph is null in insertVertex().\n");
         return;
     }
-    *nodeList = (Node **)realloc(*nodeList, *size);
-    if(*nodeList == NULL) {
-        printf("error: Failed to reallocate memory in insertNodeList.\n");
+    if(isExistVertex(graph, value)) {
+        printf("Function: %c already exists. Dumped value. insertVertex()).\n", value);
         return;
     }
-    *(*nodeList + *size ) = createNode(value);
-    (*size)++;
-    //nodeList[*size] = createNode(value);
+    (graph->numOfVertices)++;
+    graph->vertices = (Vertex **)realloc(graph->vertices, (graph->numOfVertices) * sizeof(Vertex *));
+    (graph->vertices)[(graph->numOfVertices) - 1] = createVertex(value);
+}
+
+
+void connectVertices(Graph * graph, char start, char dest, int weight) {
+    if(graph == NULL) {
+        printf("Error: graph is null when connectVertices().\n");
+        return;
+    }
+    if(!(isExistVertex(graph, start) && isExistVertex(graph, dest))) {
+        printf("Error: either vertex %c or %c does not exist when connectVertices().\n", start, dest);
+        return;
+    }
+    if(isExistEdge(graph, (searchVertex(graph, start))->value, dest)) {
+        printf("Function: edge from %c to %c already exists. Dumping edge. connectVertices().\n", start, dest);
+        return;
+    }
+    Vertex * vertex = searchVertex(graph, start);
+    if(vertex == NULL) {
+        printf("Error: failed to return vertex when connectVertices().\n");
+        return;
+    }
+    if(vertex->edges == NULL) {
+        vertex->edges = createEdge(dest, weight);
+        (vertex->numOfEdges)++;
+        return;
+    }
+    Edge * curr = vertex->edges;
+    while(curr->next != NULL)
+        curr = curr->next;
+    
+    curr->next = createEdge(dest, weight);
     
 }
 
-void displayAllNodes(Node ** nodes, int size) {
-    int i;
-    for(i = 0; i < size; i++) {
-        if((i + 1) != size)
-            printf("%c, ", nodes[i]->value);
-        else
-            printf("%c\n", nodes[i]->value);
+void printVertices(Graph * graph) {
+    if(graph == NULL) {
+        printf("Error: graph is null when printVertices().\n");
+        return;
     }
+    printf("Vertices: ");
+    int i;
+    for(i = 0; i < graph->numOfVertices; i++)
+        printf("%c ", (graph->vertices)[i]->value);
+    printf("\n");
 }
 
+void printEdges(Graph * graph, char vertexTarget) {
+    if(graph == NULL) {
+        printf("Error: graph is null when printEdges().\n");
+        return;
+    }
+    Vertex * vertex = searchVertex(graph, vertexTarget);
+    Edge * start = vertex->edges;
+    printf("Vertex %c: ", vertexTarget);
+    while(start != NULL) {
+        printf("%c, ", start->dest);
+        start = start->next;
+    }
+    printf("\n");
+}
 
+bool isVisited(char * visited, char node, int count) {
+    int i;
+    for(i = 0; i < count; i++) {
+        if(visited[i] == node)
+            return true;
+    }
+    return false;
+}
+
+// depth first search
+void dfs(Graph * graph, char start) {
+    int visitCount = 0;
+    char * visited = (char *)malloc(sizeof(char));
+    Vertex * vertex = searchVertex(graph, start);
+    if(vertex == NULL) {
+        printf("Error: start vertex %c does not exist. dfs()\n", start);
+        return;
+    }
+    Edge * edgeStart = NULL;
+
+    while(visitCount < graph->numOfVertices) {
+        if(!isVisited(visited, vertex->value, visitCount)) {
+            visitCount++;
+            visited = (char *)realloc(visited, visitCount * sizeof(char));
+            visited[visitCount - 1] = vertex->value;
+            edgeStart = vertex->edges;
+            while(edgeStart != NULL) {
+                if(!isVisited(visited, edgeStart->dest, visitCount)) {
+                    vertex = searchVertex(graph, edgeStart->dest);
+                    break;
+                }
+                edgeStart = edgeStart->next;
+            }
+        }
+    }
+
+    int i;
+    for(i = 0; i < visitCount; i++)
+        printf("%c, ", visited[i]);
+}
 
 int main() {
+    Graph * graph = createGraph();
+    insertVertex(graph, 'A');
+    insertVertex(graph, 'B');
+    insertVertex(graph, 'C');
+    insertVertex(graph, 'D');
+    insertVertex(graph, 'E');
+    insertVertex(graph, 'F');
+    insertVertex(graph, 'G');
+    
+    printVertices(graph);
 
-    int size = 0;
-    Node ** nodeList = NULL;
-    insertNodeList(&nodeList, &size, 'A');
-    insertNodeList(&nodeList, &size, 'B');
-    insertNodeList(&nodeList, &size, 'C');
-    displayAllNodes(nodeList, size);
+    connectVertices(graph, 'A', 'B', 1);
+    connectVertices(graph, 'A', 'C', 1);
+    connectVertices(graph, 'B', 'G', 1);
+    connectVertices(graph, 'B', 'A', 1);
+    connectVertices(graph, 'C', 'A', 1);
+    connectVertices(graph, 'C', 'G', 1);
+    connectVertices(graph, 'C', 'F', 1);
+    connectVertices(graph, 'D', 'E', 1);
+    connectVertices(graph, 'E', 'D', 1);
+    connectVertices(graph, 'E', 'F', 1);
+    connectVertices(graph, 'F', 'C', 1);
+    connectVertices(graph, 'F', 'G', 1);
+    connectVertices(graph, 'F', 'E', 1);
+    connectVertices(graph, 'G', 'B', 1);
+    connectVertices(graph, 'G', 'F', 1);
+    connectVertices(graph, 'G', 'C', 1);
 
-    return 0;
+    printEdges(graph, 'A');
+    printEdges(graph, 'B');
+    printEdges(graph, 'C');
+    printEdges(graph, 'D');
+    printEdges(graph, 'E');
+    printEdges(graph, 'F');
+    printEdges(graph, 'G');
+
+    dfs(graph, 'A');
 }
---
+++ ite14_files/project/main.c
@@ -0,0 +1,531 @@
+// compile this using this command: gcc main.c -lm
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define CHECK "\xE2\x9C\x93"
+#define WRONG "\xE2\x9C\x97"
+
+// clears screen
+void clearScr() {
+    printf( "\033[H\033[2J" );
+}
+
+void clearStdIn() {
+    // Source - https://stackoverflow.com/a/7898516
+    // Posted by jamesdlin, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-05-05, License - CC BY-SA 4.0
+
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+typedef struct Node {
+    int data;
+    int level;
+    struct Node * left;
+    struct Node * right;
+} Node;
+
+typedef struct QueueNode {
+    Node * node;
+    struct QueueNode * next;
+} QueueNode;
+
+// For level order transversal
+QueueNode * createQueueNode(Node * node) {
+    QueueNode * new = (QueueNode *)malloc(sizeof(QueueNode));
+    if(new == NULL)
+        return NULL;
+    new->node = node;
+    new->next = NULL;
+}
+
+Node * createNode(int data) {
+    Node * newNode = (Node *)malloc(sizeof(Node));
+    if(newNode == NULL)
+        return NULL;
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// For level order traversal
+void enqueue(QueueNode ** front, Node * node) {
+    if(node == NULL) {
+        return;
+    }
+    QueueNode * newNode = createQueueNode(node);
+    if(*front == NULL) {
+        *front = newNode;
+        return;
+    }
+    QueueNode * curr = *front;
+
+    while(curr->next != NULL)
+        curr = curr->next;
+    
+    curr->next = newNode;
+}
+
+// For level order traversal
+Node * dequeue(QueueNode ** front) {
+    if(*front == NULL) return NULL;
+    
+    QueueNode * toDelete = *front;
+    Node * returnNode = toDelete->node;
+    *front = toDelete->next;
+    free(toDelete);
+    return returnNode;
+}
+
+
+Node * insertNode(Node * root, int data, int level) {
+    if(root == NULL){
+        Node * newNode = createNode(data);
+        newNode->level = level;
+        return newNode;
+    }
+    root->level = level;
+    if(data < root->data) {
+        root->left = insertNode(root->left, data, level + 1);
+    }
+    else if(data > root->data) {
+        root->right = insertNode(root->right, data, level + 1);
+    }
+    return root;
+}
+
+
+int getHeight(Node * root) {
+    if(root == NULL)
+        return 0;
+    if(root->left == NULL && root->right == NULL)
+        return root->level;
+    
+    int left = getHeight(root->left);
+    int right = getHeight(root->right);
+
+    if(left > right)
+        return left;
+    else
+        return right;
+}
+
+void addNode(Node ** root, int * nodeCount) {
+    int numberOfNodes, data;
+    int i;
+    int failedInserts = 0;
+
+    clearScr();
+    printf("< ADD NODES >\n\n");
+    printf("Enter how many nodes to insert: ");
+    scanf("%d", &numberOfNodes);
+
+    for(i = 0; i < numberOfNodes; i++) {
+        printf("Enter number: ");
+        if(scanf("%d", &data)) {
+            if(*root == NULL) {
+                *root = insertNode(*root, data, 0);
+                continue;
+            }
+            insertNode(*root, data, 0);
+        }
+        else {
+            failedInserts++;
+            continue;
+        }
+    }
+    *nodeCount += numberOfNodes;
+}
+
+void displayTree(Node * root) {
+    if(root == NULL )
+        return;
+    printf("data: %d | level: %d\n", root->data, root->level);
+    displayTree(root->left);
+    displayTree(root->right);
+}
+
+
+
+void inorder(Node * root) {
+    if(root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("[ %d ]", root->data);
+    inorder(root->right);
+}
+
+void preorder(Node * root) {
+    if(root == NULL)
+        return;
+    printf("[ %d ]", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node * root) {
+    if(root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("[ %d ]", root->data);
+}
+
+void levelOrder(Node * root, int nodeCount) {
+    QueueNode * queue = NULL;
+    Node * dequeuedNode = NULL;
+    int * visitedNodes = (int *)calloc(nodeCount, sizeof(int));
+    int i = 0; int j = 0;
+    enqueue(&queue, root);
+    while(queue != NULL) {
+        dequeuedNode = dequeue(&queue);
+        if(dequeuedNode == NULL)
+            continue;
+        visitedNodes[i] = dequeuedNode->data;
+        enqueue(&queue, dequeuedNode->left);
+        enqueue(&queue, dequeuedNode->right);
+        i++;
+    }
+    
+    for(; j < nodeCount; j++)
+        printf("[ %d ]", visitedNodes[j]);
+    free(visitedNodes);
+}
+
+
+void traversalSubMenu(Node * root, int nodeCount) {
+    int choice;
+    int isRunning = 1;
+    while(isRunning) {
+        clearScr();
+        printf("< TRAVERSAL OPTIONS >\n\n");
+        printf("[1] Inorder Traversal\n[2] Preorder Traversal\n[3] Postorder Traversal\n[4] Level Order Traversal\n[5] Back to Main Menu\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        if(root == NULL && choice != 5) {
+            clearScr();
+            clearStdIn();
+            printf("< TRAVERSAL OPTIONS >\n\n");
+            printf("There is no tree to traverse.\n");
+            printf("\n[press any key to continue].\n");
+            getchar();
+            continue;
+        }
+
+        switch(choice) {
+            case 1:
+                clearScr();
+                clearStdIn();
+                printf("< INORDER TRANSVERSAL >\n\n");
+                inorder(root);
+                printf("\n[press any key to continue].\n");
+                getchar();
+                break;
+            case 2:
+                clearScr();
+                clearStdIn();
+                printf("< PREORDER TRANSVERSAL >\n\n");
+                preorder(root);
+                printf("\n[press any key to continue].\n");
+                getchar();
+                break;
+            case 3:
+                clearScr();
+                clearStdIn();
+                printf("< POSTORDER TRANSVERSAL >\n\n");
+                postorder(root);
+                printf("\n[press any key to continue].\n");
+                getchar();
+                break;
+            case 4:
+                clearScr();
+                clearStdIn();
+                printf("<LEVEL ORDER TRANSVERSAL>\n\n");
+                levelOrder(root, nodeCount);
+                printf("\n[press any key to continue].\n");
+                getchar();
+                break;
+            case 5:
+                isRunning = 0;
+                break;
+            default:
+                printf("Invalid Input.\n");
+        }
+    }
+}
+
+int search(Node * root, int target) {
+    if(root == NULL) 
+        return 0;
+    
+    if(root->data == target)
+        return 1;
+    
+    if(target > root->data) {
+        return 1 && search(root->right, target);
+    }
+    else if(target < root->data) {
+        return 1 && search(root->left, target);
+    }
+}
+
+void searchValue(Node * root) {
+    int value;
+    clearScr();
+    printf("< SEARCH VALUE >\n\n");
+    if(root == NULL) {
+        printf("There is no tree.\n");
+    }
+    else {
+        printf("Enter value: ");
+        scanf("%d", &value);
+        printf("%d %s\n", value, (search(root, value)) ? "exists in the tree" : "does not exist in the tree");
+    }
+    clearStdIn();
+    printf("\n\n[press any key to continue]\n");
+    getchar();
+}
+// nodes have 2 or 0 children
+int isStrictBST(Node * root) {
+    QueueNode * queue = NULL;
+    Node * node = NULL;
+    enqueue(&queue, root);
+    while(queue != NULL) {
+        node = dequeue(&queue);
+        if(node->left == NULL && node->right == NULL)
+            continue;
+        if(node->left == NULL || node->right == NULL)
+            return 0;
+        enqueue(&queue, node->left);
+        enqueue(&queue, node->right);
+    }
+    return 1;
+} 
+//every level is filled except possibly at the last level (must be left filled)
+int isCompleteBST(Node * root, int height) {
+    int totalNodes = (int)pow(2, height + 1) - 1;
+    QueueNode * queue = NULL;
+    Node * nullNode = createNode(-1);
+    Node * node = NULL;
+    int * visited = (int *)calloc(totalNodes, sizeof(int));
+    int i = 0;
+    enqueue(&queue, root);
+    while(queue != NULL) {
+        node = dequeue(&queue);
+        if(node == NULL)
+            continue;
+        visited[i++] = node->data;
+        if(node->left == NULL && node->right == NULL)
+            continue;
+        if(node->left == NULL)
+            enqueue(&queue, nullNode);
+        else
+            enqueue(&queue, node->left);
+        if(node->right == NULL)
+            enqueue(&queue, nullNode);
+        else
+            enqueue(&queue, node->right);
+    }
+
+    for(i = 0; i < totalNodes; i++) {
+        if(visited[i] < 0) {
+            free(visited);
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// everything is filled
+int isFullBST(Node * root, int height) {
+    if(!isStrictBST(root))
+        return 0;
+    int totalNodes = (int)pow(2, height + 1) - 1;
+    QueueNode * queue = NULL;
+    Node * nullNode = createNode(-1);
+    Node * node = NULL;
+    int * visited = (int *)calloc(totalNodes, sizeof(int));
+    int i = 0;
+    enqueue(&queue, root);
+    while(queue != NULL) {
+        node = dequeue(&queue);
+        if(node == NULL)
+            continue;
+        visited[i++] = node->data;
+        if(node->left == NULL && node->right == NULL)
+            continue;
+        enqueue(&queue, node->left);
+        enqueue(&queue, node->right);
+    }
+
+    for(i = 0; i < totalNodes; i++) {
+        if(visited[i] == 0) {
+            free(visited);
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+void treeType(Node * root, int height) {
+    clearScr();
+    printf("< TREE TYPE >\n\n");
+
+    if(root) {
+        printf("strict binary tree: \t\t%s\n", isStrictBST(root) ? CHECK : WRONG);
+        printf("complete binary tree: \t\t%s\n", isCompleteBST(root, height) ? CHECK : WRONG); 
+        printf("full binary tree: \t\t%s\n", isFullBST(root, height) ? CHECK : WRONG);
+    }
+    else {
+        printf("There is no tree.\n");
+    }
+    clearStdIn();
+    printf("\n[press any key to continue]\n");
+    getchar();
+}
+
+void printLeaves(Node * root) {
+    if(root == NULL)
+        return;
+    if(root->left == NULL && root->right == NULL) {
+        printf("[ %d ]", root->data);
+        return;
+    }
+    printLeaves(root->left);
+    printLeaves(root->right);
+}
+
+void displayLeaves(Node * root) {
+    clearScr();
+    printf("< DISPLAY LEAVES >\n\n");
+    if(root == NULL)
+        printf("There is no tree.\n");
+    else
+        printLeaves(root);
+    printf("\n\n[press any key to continue]\n");
+    clearStdIn();
+    getchar();
+}
+
+Node * getParent(Node * root, int target) {
+    if(root == NULL)
+        return NULL;
+    if(root->data == target)
+        return NULL;
+    while(1) {
+        if(root->left == NULL && root->right == NULL)
+            return NULL;
+        if(root->left != NULL) {
+            if(root->left->data == target)
+                return root;
+        }
+        if(root->right != NULL) {
+            if(root->right->data == target)
+                return root;
+        }
+        if(target > root->data)
+            root = root->right;
+        else if(target < root->data)
+            root = root->left;
+    }
+}
+
+
+void displaySiblings(Node * root) {
+    clearScr();
+    printf("< DISPLAY SIBLINGS >\n\n");
+    if(root == NULL) {
+        printf("There is no tree.\n");
+        clearStdIn();
+        printf("\n[press any key to continue]\n");
+        getchar();
+        return;
+    }
+    int target;
+    int isFound;
+    Node * parent = NULL;
+    printf("Enter value: ");
+    scanf("%d", &target);
+    if(!search(root, target)) {
+        printf("%d does not exist in the tree.\n", target);
+        clearStdIn();
+        printf("\n[press any key to continue]\n");
+        getchar();
+        return;
+    }
+    parent = getParent(root, target);
+    if(parent == NULL) {
+        printf("Parent =>\tNULL\n");
+        printf("Target =>\tdata : %d | level : %d\n", target, 0);
+        printf("[No Siblings]\n");
+    }
+    else {
+        printf("Parent =>\tdata : %d | level : %d\n", parent->data, parent->level);
+        printf("Target =>\tdata : %d | level : %d\n", target, (parent->level) + 1);
+        if(parent->left != NULL && parent->right != NULL) 
+            printf("Sibling =>\tdata : %d | level : %d\n", (parent->left->data != target) ? parent->left->data : parent->right->data, (parent->level) + 1);
+        else
+            printf("[No Siblings]\n");
+    }
+    clearStdIn();
+    printf("\n[press any key to continue]\n");
+    getchar();
+}
+
+void displayMenu(Node ** root) {
+    int choice;
+    int isRunning = 1;
+    int nodeCount = 0;
+    int treeHeight = 0;
+
+    while(isRunning) {
+        clearScr();
+        displayTree(*root);
+        printf("< MAIN MENU >\n\n");
+        printf("[1] Transversal Operations\n[2] Add Nodes\n[3] Search Value\n[4] Determine Tree Type\n[5] Display Leaves\n[6] Display Siblings\n[7] Exit\n");
+        printf("Enter Option: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                traversalSubMenu(*root, nodeCount);
+                break;
+            case 2:
+                addNode(&(*root), &nodeCount);
+                break;
+            case 3:
+                searchValue(*root);
+                break;
+            case 4:
+                treeType(*root, treeHeight);
+                break;
+            case 5:
+                displayLeaves(*root);
+                break;
+            case 6:
+                displaySiblings(*root);
+                break;  
+            case 7:
+                isRunning = 0;
+                break;
+            default:
+                printf("Invalid input.\n");
+        }
+        treeHeight = getHeight(*root);
+    }
+}
+
+
+int main() {
+    Node * root = NULL;
+    displayMenu(&root);
+    clearScr();
+    printf("Bye.\n"); 
+    return 0;
+}
